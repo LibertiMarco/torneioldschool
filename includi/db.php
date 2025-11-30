18eba6400c50e8912d1b05d9104121fb02e4cedc
LibertiMarco <Marco01liberti@icloud.com>
2025-11-30 19:21:24 +0100

    fix

--- includi/db.php
+++ includi/db.php
@@ -16,10 +16,20 @@ if ($user === '' || $dbname === '') {
     die('Configurazione DB mancante: definisci DB_USER e DB_NAME.');
 }
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+// Evita eccezioni fatali su connessione fallita: gestiamo noi l'errore
+mysqli_report(MYSQLI_REPORT_OFF);
+try {
+    $conn = @new mysqli($host, $user, $pass, $dbname);
+} catch (mysqli_sql_exception $e) {
+    error_log('Connessione DB fallita: ' . $e->getMessage());
+    http_response_code(500);
+    exit('Connessione al database non disponibile.');
+}
 
 if ($conn->connect_error) {
-    die("Connessione fallita: " . $conn->connect_error);
+    error_log('Connessione DB fallita: ' . $conn->connect_error);
+    http_response_code(500);
+    exit('Connessione al database non disponibile.');
 }
 
 $conn->set_charset("utf8mb4");
