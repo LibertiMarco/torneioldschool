4af1db033876790c3f02e28ef68867218ca8fdd1
LibertiMarco <Marco01liberti@icloud.com>
2026-02-06 14:57:16 +0100

    fix login

--- logout.php
+++ logout.php
@@ -2,6 +2,14 @@
 session_start();
 $userId = $_SESSION['user_id'] ?? null;
 
+$cookieDomain = '';
+if (!empty($_SERVER['HTTP_HOST'])) {
+    $host = strtolower((string)explode(':', $_SERVER['HTTP_HOST'])[0]);
+    if ($host !== 'localhost' && !filter_var($host, FILTER_VALIDATE_IP)) {
+        $cookieDomain = preg_replace('/^www\./', '', $host);
+    }
+}
+
 if ($userId) {
     $dbPath = __DIR__ . '/includi/db.php';
     if (file_exists($dbPath)) {
@@ -22,9 +30,14 @@ session_destroy();
 
 if (ini_get('session.use_cookies')) {
     $params = session_get_cookie_params();
+    $domain = $params['domain'] ?? '';
+    if ($domain === '') {
+        $domain = $cookieDomain;
+    }
+
     setcookie(session_name(), '', time() - 42000, [
         'path' => $params['path'] ?? '/',
-        'domain' => $params['domain'] ?? '',
+        'domain' => $domain,
         'secure' => (bool)($params['secure'] ?? false),
         'httponly' => (bool)($params['httponly'] ?? true),
         'samesite' => $params['samesite'] ?? 'Lax',
@@ -33,7 +46,7 @@ if (ini_get('session.use_cookies')) {
     $rememberCookieName = defined('REMEMBER_COOKIE_NAME') ? REMEMBER_COOKIE_NAME : 'tos_keep_login';
     setcookie($rememberCookieName, '', time() - 42000, [
         'path' => $params['path'] ?? '/',
-        'domain' => $params['domain'] ?? '',
+        'domain' => $domain,
         'secure' => (bool)($params['secure'] ?? false),
         'httponly' => true,
         'samesite' => $params['samesite'] ?? 'Lax',
