141314557c6aa9d66c112c653117d1b63cc2d1cd
LibertiMarco <Marco01liberti@icloud.com>
2026-03-06 18:19:18 +0100

    fix

--- tornei/AllInOneNightMondiale.php
+++ tornei/AllInOneNightMondiale.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/../includi/require_login.php';
 require_once __DIR__ . '/../includi/seo.php';
 
-$assetVersion = '20260206';
+$assetVersion = '20260306';
 $baseUrl = seo_base_url();
 $torneoSeo = [
   'title' => "All in one night: Mondiale - Tornei Old School",
@@ -547,5 +547,4 @@ $torneoSeo = [
 
 </body>
 </html>
-
 
